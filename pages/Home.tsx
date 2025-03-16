f1bd9a432ad5b78d0fa6e07f6fb7d790b6518574
alexjosf <edavvealex@gmail.com>
2025-03-16 09:55:39 -0400

    Final
    Done

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -20,19 +20,25 @@ function Home() {
       <div className='flex flex-row justify-center p-18'>
         <h1 className='font-medium text-xl text-end'
         >Discover Events happening near<br /> you or whichever country</h1>
-          <img src='globe.png'
-            className='h-12 w-12 mx-20'
-          />
+        <img src='globe.png'
+          className='h-12 w-12 mx-20'
+        />
         <h1 className='font-medium text-xl'
         >Discover artists Nearby you or around <br />the globe</h1>
       </div>
-      <div>
-      <img src='merlin.webp'
-            className='h-100 w-120 rounded-2xl'
-          />
-          <img src='singer.jpeg'
-            className='h-100 w-120 rounded-2xl'
-          />
+      <div className='flex flex-row justify-between p-18'>
+        <img src='image1.jpg'
+          className='h-80 w-90 rounded-2xl'
+        />
+        <img src='image2.jpg'
+          className='h-80 w-80 rounded-full'
+        />
+        <img src='image3.jfif'
+          className='h-80 w-80 rounded-b-[75px] rounded-tl-[75px] rounded-tr-2xl'
+        />
+        <img src='image4.jpg'
+          className='h-80 rounded-full'
+        />
       </div>
     </Layout>
   )
